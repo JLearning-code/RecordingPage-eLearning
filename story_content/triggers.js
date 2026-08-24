a159a9e9c501518ba1b9f2715fe773c53713570c
JLearning-code <salgadojenny0000@gmail.com>
2026-08-24 23:04:02 +0800

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,44 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "66k5sQReZD8":
+        Script1();
+        break;
+      case "5lFYaqWr3v3":
+        Script2();
+        break;
+      case "6lkUVQnCDN3":
+        Script3();
+        break;
+      case "5nrnYMrNIiy":
+        Script4();
+        break;
+      case "5nurz9Mjklr":
+        Script5();
+        break;
+      case "6k667ccOxVc":
+        Script6();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
